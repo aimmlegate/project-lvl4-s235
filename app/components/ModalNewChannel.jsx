fee74aa632843d14cba181a9950c67eff01d972d
Andrey Lukin <aimm.legate@gmail.com>
2018-06-07 18:11:50 +0500

    fix

--- app/components/ModalNewChannel.jsx
+++ app/components/ModalNewChannel.jsx
@@ -1,40 +1,60 @@
 import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, Label, Input, FormGroup} from 'reactstrap';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  Label,
+  Input,
+  FormGroup,
+} from 'reactstrap';
 
 class ModalNewChannel extends React.Component {
-  state = {name: ''};
+  state = { name: '' };
 
-  handleChange = (e) => {
+  handleChange = e => {
     this.setState({ name: e.target.value });
   };
 
   handleClose = () => {
     this.props.toggle();
-    this.setState({ name: ''});
-  }
+    this.setState({ name: '' });
+  };
 
-  handleSubmit = () => {
+  handleSubmit = (e) => {
+    e.preventDefault();
     this.props.submit(this.state.name);
     this.props.toggle();
-    this.setState({ name: ''});
-  }
+    this.setState({ name: '' });
+  };
 
   render() {
     return (
       <div>
         <Modal isOpen={this.props.isOpen}>
-          <ModalHeader toggle={this.handleClose}>New Channel</ModalHeader>
-          <ModalBody>
-            <Form>
-            <FormGroup>
-              <Label for="exampleEmail">Channel name</Label>
-              <Input type="text" name="channelName" placeholder="Enter name" value={this.state.name} onChange={this.handleChange}/>
-            </FormGroup>
-            </Form>
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.handleSubmit}>Create</Button>
-          </ModalFooter>
+          <Form onSubmit={this.handleSubmit}>
+            <ModalHeader toggle={this.handleClose}>New Channel</ModalHeader>
+            <ModalBody>
+              <FormGroup>
+                <Label for="exampleEmail">Channel name</Label>
+                <Input
+                  type="text"
+                  name="channelName"
+                  placeholder="Enter name"
+                  value={this.state.name}
+                  onChange={this.handleChange}
+                  required
+                />
+              </FormGroup>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" type="submit">
+                Create
+              </Button>
+            </ModalFooter>
+          </Form>
         </Modal>
       </div>
     );
